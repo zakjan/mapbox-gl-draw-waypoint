efcd99c6226f262324b84b56a21db88f425d2384
Jan Zak <zj@zakjan.cz>
2020-08-13 07:35:36 +0200

    Fix typo

--- src/modes/index.js
+++ src/modes/index.js
@@ -5,7 +5,7 @@ import patchDirectSelect from './direct_select';
 export function enable(modes, preventFunction = () => true) {
   return {
     ...modes,
-    [Constants.modes.SIMPLE_SELECT]: patchSimpleSelect(modes[Constants.modes.SIMPLE_SELECT], preventFunction),
-    [Constants.modes.DIRECT_SELECT]: patchDirectSelect(modes[Constants.modes.DIRECT_SELECT])
+    [Constants.modes.SIMPLE_SELECT]: patchSimpleSelect(modes[Constants.modes.SIMPLE_SELECT]),
+    [Constants.modes.DIRECT_SELECT]: patchDirectSelect(modes[Constants.modes.DIRECT_SELECT], preventFunction)
   };
 }
